cf775a4ded8615f9277a75c40acc1fa6c47ade1e
MaaviaAsghar4 <maavia.asghar.96@gmail.com>
2020-12-28 10:57:51 +0500

    bookmark app completed

--- functions/graphql-bookmark/graphql-bookmark.js
+++ functions/graphql-bookmark/graphql-bookmark.js
@@ -1,42 +1,109 @@
-const { ApolloServer, gql } = require('apollo-server-lambda')
+const { ApolloServer, gql } = require("apollo-server-lambda");
+const faundadb = require("faunadb");
+const q = faundadb.query;
+const dotenv = require("dotenv");
+dotenv.config();
 
 const typeDefs = gql`
   type Query {
-    hello: String
-    allAuthors: [Author!]
-    author(id: Int!): Author
-    authorByName(name: String!): Author
+    bookmarks: [Bookmarks]!
   }
-  type Author {
+  type Mutation {
+    addBookmark(title: String!, link: String!, imgURL: String!): Bookmarks
+    deleteBookmark(id: ID!): Bookmarks
+  }
+  type Bookmarks {
     id: ID!
-    name: String!
-    married: Boolean!
+    title: String!
+    link: String!
+    imgURL: String!
   }
-`
-
-const authors = [
-  { id: 1, name: 'Terry Pratchett', married: false },
-  { id: 2, name: 'Stephen King', married: true },
-  { id: 3, name: 'JK Rowling', married: false },
-]
+`;
 
 const resolvers = {
   Query: {
-    hello: () => 'Hello, world!',
-    allAuthors: () => authors,
-    author: () => {},
-    authorByName: (root, args) => {
-      console.log('hihhihi', args.name)
-      return authors.find((author) => author.name === args.name) || 'NOTFOUND'
+    bookmarks: async (roots, args, context) => {
+      try {
+        const adminClient = new faundadb.Client({
+          secret: process.env.FAUNA_DB_SECRET_KEY,
+        });
+
+        const result = await adminClient.query(
+          q.Map(
+            q.Paginate(q.Match(q.Index("bookmark_index"))),
+            q.Lambda("X", q.Get(q.Var("X")))
+          )
+        );
+
+        console.log(result.data);
+
+        return result.data.map((value) => {
+          return {
+            id: value.ref.id,
+            title: value.data.title,
+            link: value.data.link,
+            imgURL: value.data.imgURL,
+          };
+        });
+      } catch (error) {
+        console.log(error.message);
+      }
     },
   },
-}
+
+  Mutation: {
+    addBookmark: async (_, { title, link, imgURL }) => {
+      try {
+        const adminClient = new faundadb.Client({
+          secret: process.env.FAUNA_DB_SECRET_KEY,
+        });
+
+        const result = await adminClient.query(
+          q.Create(q.Collection("bookmark_app"), {
+            data: {
+              title,
+              link,
+              imgURL,
+            },
+          })
+        );
+
+        return {
+          id: result.ref.id,
+          title: result.ref.data.title,
+          link: result.ref.data.link,
+          imgURL: result.ref.data.imgURL,
+        };
+      } catch (error) {
+        console.log(error.message);
+      }
+    },
+    deleteBookmark: async (_, { id }) => {
+      try {
+        const adminClient = new faundadb.Client({
+          secret: process.env.FAUNA_DB_SECRET_KEY,
+        });
+
+        const result = await adminClient.query(
+          q.Delete(q.Ref(q.Collection("bookmark_app"), id))
+        );
+        console.log(result);
+        return {
+          id: result.ref.id,
+          title: result.ref.data.title,
+          link: result.ref.data.link,
+          imgURL: result.ref.data.imgURL,
+        };
+      } catch (error) {
+        console.log(error.message);
+      }
+    },
+  },
+};
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-})
+});
 
-const handler = server.createHandler()
-
-module.exports = { handler }
+exports.handler = server.createHandler();
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Container,
   TextField,
@@ -13,9 +13,86 @@ import {
   Box,
 } from "@material-ui/core";
 import styles from "./index.module.css";
-import GatsbyImag from "../images/icon.png";
+import gql from "graphql-tag";
+import { useQuery, useMutation } from "@apollo/client";
+
+interface arrtype {
+  id: string;
+  title: string;
+  link: string;
+  imgURL: string;
+  __typename: string;
+}
+
+const GET_BOOKMARKS = gql`
+  {
+    bookmarks {
+      id
+      title
+      link
+      imgURL
+    }
+  }
+`;
+
+const ADD_BOOKMARK = gql`
+  mutation addBookmark($title: String!, $link: String!, $imgURL: String!) {
+    addBookmark(title: $title, link: $link, imgURL: $imgURL) {
+      title
+      link
+      imgURL
+    }
+  }
+`;
+
+const DELETE_BOOKMARK = gql`
+  mutation deleteBookmark($id: ID!) {
+    deleteBookmark(id: $id) {
+      id
+    }
+  }
+`;
 
 const IndexPage = () => {
+  let [titleValue, setTitleValue] = useState("");
+  let [linkValue, setLinkValue] = useState("");
+  let [imgValue, setImgValue] = useState("");
+  const [addBookmark] = useMutation(ADD_BOOKMARK);
+  const [deleteBookmark] = useMutation(DELETE_BOOKMARK);
+
+  const addNewBookmark = () => {
+    addBookmark({
+      variables: {
+        title: titleValue,
+        link: linkValue,
+        imgURL: imgValue,
+      },
+      refetchQueries: [{ query: GET_BOOKMARKS }],
+    });
+    setTitleValue("");
+    setLinkValue("");
+    setImgValue("");
+  };
+
+  const delBookmark = (id: string) => {
+    deleteBookmark({
+      variables: {
+        id,
+      },
+      refetchQueries: [{ query: GET_BOOKMARKS }],
+    });
+  };
+
+  const { loading, error, data } = useQuery(GET_BOOKMARKS);
+  if (loading) {
+    return <h1>Loading..</h1>;
+  }
+
+  if (error) {
+    return <h1>{error.message}</h1>;
+  }
+
+  console.log(data);
   return (
     <div>
       <title>Bookmarking App</title>
@@ -24,6 +101,8 @@ const IndexPage = () => {
         <TextField
           type="text"
           fullWidth
+          value={titleValue}
+          onChange={(e) => setTitleValue(e.target.value)}
           className={styles.textField}
           id="title"
           label="Title"
@@ -32,6 +111,8 @@ const IndexPage = () => {
         <TextField
           type="text"
           fullWidth
+          value={linkValue}
+          onChange={(e) => setLinkValue(e.target.value)}
           className={styles.textField}
           id="url"
           label="Link"
@@ -40,12 +121,18 @@ const IndexPage = () => {
         <TextField
           type="text"
           fullWidth
+          value={imgValue}
+          onChange={(e) => setImgValue(e.target.value)}
           className={styles.textField}
           id="image"
           label="Image"
           variant="outlined"
         />
-        <Button className={styles.btn} variant="contained">
+        <Button
+          onClick={addNewBookmark}
+          className={styles.btn}
+          variant="contained"
+        >
           Add
         </Button>
       </Container>
@@ -54,138 +141,53 @@ const IndexPage = () => {
           <Typography className={styles.title}>Your Bookmarks</Typography>
         </Box>
         <Grid container spacing={3}>
-          <Grid item xs={12} sm={6} md={4}>
-            <Card>
-              <CardActionArea>
-                <CardMedia
-                  component="img"
-                  alt="Contemplative Reptile"
-                  height="140"
-                  image={GatsbyImag}
-                  title="Contemplative Reptile"
-                />
-                <CardContent>
-                  <Typography
-                    className={styles.title}
-                    gutterBottom
-                    variant="h5"
-                    component="h2"
-                  >
-                    Lizard
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-              <CardActions>
-                <Button
-                  className={styles.linkBtn}
-                  variant="contained"
-                  size="small"
-                  color="primary"
-                >
-                  <a href="#">Learn More</a>
-                </Button>
-              </CardActions>
-            </Card>
-          </Grid>
-          <Grid item xs={12} sm={6} md={4}>
-            <Card>
-              <CardActionArea>
-                <CardMedia
-                  component="img"
-                  alt="Contemplative Reptile"
-                  height="140"
-                  image={GatsbyImag}
-                  title="Contemplative Reptile"
-                />
-                <CardContent>
-                  <Typography
-                    className={styles.title}
-                    gutterBottom
-                    variant="h5"
-                    component="h2"
-                  >
-                    Lizard
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-              <CardActions>
-                <Button
-                  className={styles.linkBtn}
-                  variant="contained"
-                  size="small"
-                  color="primary"
-                >
-                  <a href="#">Learn More</a>
-                </Button>
-              </CardActions>
-            </Card>
-          </Grid>
-          <Grid item xs={12} sm={6} md={4}>
-            <Card>
-              <CardActionArea>
-                <CardMedia
-                  component="img"
-                  alt="Contemplative Reptile"
-                  height="140"
-                  image={GatsbyImag}
-                  title="Contemplative Reptile"
-                />
-                <CardContent>
-                  <Typography
-                    className={styles.title}
-                    gutterBottom
-                    variant="h5"
-                    component="h2"
-                  >
-                    Lizard
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-              <CardActions>
-                <Button
-                  className={styles.linkBtn}
-                  variant="contained"
-                  size="small"
-                  color="primary"
-                >
-                  <a href="#">Learn More</a>
-                </Button>
-              </CardActions>
-            </Card>
-          </Grid>
-          <Grid item xs={12} sm={6} md={4}>
-            <Card>
-              <CardActionArea>
-                <CardMedia
-                  component="img"
-                  alt="Contemplative Reptile"
-                  height="140"
-                  image={GatsbyImag}
-                  title="Contemplative Reptile"
-                />
-                <CardContent>
-                  <Typography
-                    className={styles.title}
-                    gutterBottom
-                    variant="h5"
-                    component="h2"
-                  >
-                    Lizard
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-              <CardActions>
-                <Button
-                  className={styles.linkBtn}
-                  variant="contained"
-                  size="small"
-                  color="primary"
-                >
-                  <a href="#">Learn More</a>
-                </Button>
-              </CardActions>
-            </Card>
-          </Grid>
+          {data &&
+            data.bookmarks.map((bookmark: arrtype) => {
+              return (
+                <Grid key={bookmark.id} item xs={12} sm={6} md={4}>
+                  <Card>
+                    <CardActionArea>
+                      <CardMedia
+                        component="img"
+                        alt="Contemplative Reptile"
+                        height="140"
+                        image={bookmark.imgURL}
+                        title="Contemplative Reptile"
+                      />
+                      <CardContent>
+                        <Typography
+                          className={styles.title}
+                          gutterBottom
+                          variant="h5"
+                          component="h2"
+                        >
+                          {bookmark.title}
+                        </Typography>
+                      </CardContent>
+                    </CardActionArea>
+                    <CardActions>
+                      <Button
+                        className={styles.linkBtn}
+                        variant="contained"
+                        size="small"
+                        color="primary"
+                      >
+                        <a href={bookmark.link}>Visit</a>
+                      </Button>
+                      <Button
+                        className={styles.btn}
+                        variant="contained"
+                        size="small"
+                        color="primary"
+                        onClick={() => delBookmark(bookmark.id)}
+                      >
+                        Delete
+                      </Button>
+                    </CardActions>
+                  </Card>
+                </Grid>
+              );
+            })}
         </Grid>
       </Container>
     </div>
